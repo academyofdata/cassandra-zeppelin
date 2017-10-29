ba5f0d01c547d5a6d16d46159d0a8b7c10e74638
fixone <fixone@users.noreply.github.com>
2017-10-29 11:18:32 +0200

    Update gcloud-zeppelin.sh

--- gcloud-zeppelin.sh
+++ gcloud-zeppelin.sh
@@ -5,3 +5,10 @@ SID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | tr '[:upper:]' '[:lowe
 INSTANCE="zep-${SID}"
 
 gcloud compute instances create ${INSTANCE} --zone ${ZONE} --machine-type g1-small --maintenance-policy "MIGRATE" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "${INSTANCE}disk"
+
+echo "waiting for the machine to come up"
+sleep 30
+
+echo "installing cassandra on remote node"
+gcloud compute ssh ${INSTANCE} --zone $ZONE --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-zeppelin/master/zeppelin.sh | bash"
+
