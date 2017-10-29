aecd184e70d935aac6f2d19fed97d78359f594a6
fixone <fixone@users.noreply.github.com>
2017-10-29 11:23:24 +0200

    Update zeppelin.sh

--- zeppelin.sh
+++ zeppelin.sh
@@ -4,9 +4,13 @@ ZEP_VER="0.7.3"
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+echo "installing Oracle JDK"
 sudo apt-get install -y oracle-java8-installer wget
+echo "getting Zeppeling Archive"
 wget http://apache.javapipe.com/zeppelin/zeppelin-${ZEP_VER}/zeppelin-${ZEP_VER}-bin-all.tgz
+echo "unarchiving..."
 tar -xzf zeppelin-${ZEP_VER}-bin-all.tgz
-cd cd zeppelin-${ZEP_VER}-bin-all/
+cd zeppelin-${ZEP_VER}-bin-all/
+echo "starting daemon..."
 bin/zeppelin-daemon.sh start
 	
