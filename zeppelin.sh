da8fe56bc9470918079b27eb5454eff1288b032f
fixone <fixone@users.noreply.github.com>
2017-10-29 12:03:39 +0200

    Update zeppelin.sh

--- zeppelin.sh
+++ zeppelin.sh
@@ -1,6 +1,8 @@
 #script that installs zeppelin with all dependencies and starts it
 ZEP_VER="0.7.3"
 
+PASSWORD='my-pass'
+
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
@@ -11,6 +13,16 @@ wget http://apache.javapipe.com/zeppelin/zeppelin-${ZEP_VER}/zeppelin-${ZEP_VER}
 echo "unarchiving..."
 tar -xzf zeppelin-${ZEP_VER}-bin-all.tgz
 cd zeppelin-${ZEP_VER}-bin-all/
+#enable authentication
+cp conf/shiro.ini.template conf/shiro.ini
+#the Apache shiro template comes with a bunch of users pre-defined, remove them
+sed -i "/^user/d" ./conf/shiro.ini 
+# admin default password in shiro.ini is password1, change it to a value of our own
+sed "s/password1/${PASSWORD}/g" shiro.ini
+
+cp conf/zeppelin-site.xml.template conf/zeppelin-site.xml
+#disable anonymous access
+sed -i '/zeppelin.anonymous.allowed/{n;s/.*/<value>false<\/value>/}' conf/zeppelin-site.xml
 echo "starting daemon..."
 bin/zeppelin-daemon.sh start
 	
