9a721d48ac04e371f131f980a87b0e3636c2287a
fixone <fixone@users.noreply.github.com>
2017-10-29 11:24:26 +0200

    Update gcloud-zeppelin.sh

--- gcloud-zeppelin.sh
+++ gcloud-zeppelin.sh
@@ -7,7 +7,7 @@ INSTANCE="zep-${SID}"
 gcloud compute instances create ${INSTANCE} --zone ${ZONE} --machine-type g1-small --maintenance-policy "MIGRATE" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "${INSTANCE}disk"
 
 echo "waiting for the machine to come up"
-sleep 15
+sleep 25
 
 echo "installing Apache Zeppelin on remote node"
 gcloud compute ssh ${INSTANCE} --zone $ZONE --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-zeppelin/master/zeppelin.sh | bash"
