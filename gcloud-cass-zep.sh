78d9452af02be7b9dc99723fb00639b8b7e9f456
fixone <fixone@users.noreply.github.com>
2017-12-10 18:31:18 +0200

    Update gcloud-cass-zep.sh

--- gcloud-cass-zep.sh
+++ gcloud-cass-zep.sh
@@ -6,6 +6,7 @@ safeRun() {
   echo cmnd=$cmnd
   eval $cmnd
   ret_code=$?
+  echo "command ended with $ret_code"
   if [ $ret_code != 0 ]; then
     printf "Error : [%d] when executing command: '$cmnd'" $ret_code
     exit $ret_code
