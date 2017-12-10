66b31f79e4519c9543d2aac208fda38cc5d160bb
fixone <fixone@users.noreply.github.com>
2017-12-10 17:11:18 +0200

    Create gcloud-cass+zep.sh

---
+++ gcloud-cass+zep.sh
@@ -0,0 +1,10 @@
+if [ $# -lt 1 ]
+then
+	echo "Please provide an argument to this script -> the name of the node to spin up"
+	exit
+fi
+
+wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-cluster/master/gcloud-server-setup.sh | bash -s $1
+gcloud compute ssh $1 --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-cluster/master/get-data.sh | sudo bash"
+gcloud compute ssh $1 --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-zeppelin/master/zeppelin.sh | sudo bash"
+gcloud compute firewall-rules create allow-zep --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:8080 --source-ranges=0.0.0.0/0
