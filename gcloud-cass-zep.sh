6369249c576e79e41d6ac4b860a2839e15168a41
fixone <fixone@users.noreply.github.com>
2017-12-10 17:18:15 +0200

    Update gcloud-cass-zep.sh

--- gcloud-cass-zep.sh
+++ gcloud-cass-zep.sh
@@ -8,6 +8,6 @@ wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-cluster/mast
 gcloud compute ssh $1 --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-cluster/master/get-data.sh | sudo bash"
 gcloud compute ssh $1 --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-zeppelin/master/zeppelin.sh | sudo bash"
 
-gcloud compute ssh $1 --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-zeppelin/master/gcloud-user.sh | sudo bash"
+gcloud compute ssh $1 --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-zeppelin/master/gcloud-user.sh | bash -s cuser"
 
 gcloud compute firewall-rules create allow-zep --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:8080 --source-ranges=0.0.0.0/0
