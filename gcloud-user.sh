6029ee447f89eb84f09c3327c8eab5799df4e25f
fixone <fixone@users.noreply.github.com>
2018-03-08 20:59:00 +0200

    Update gcloud-user.sh

--- gcloud-user.sh
+++ gcloud-user.sh
@@ -1,3 +1,9 @@
+if [ $# -lt 1 ]
+then
+  echo "Need at least one param - user to add
+  exit
+fi
+
 echo "enabling Password Login"
 sudo sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
@@ -7,7 +13,12 @@ sudo /etc/init.d/ssh reload
 echo "adding user '$1'"
 sudo adduser $1 --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 
-PASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+if [ $# -gt 2 ]
+then
+	PASSWD=$2
+else
+  PASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+fi
 echo "setting user password to: $PASSWD"
 echo "$1:$PASSWD" | sudo chpasswd
 
